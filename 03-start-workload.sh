3bf3a72d1554cb844a681117123f4e7fe5366033
Evan Gilman <evan@scytale.io>
2019-11-14 16:44:14 -0800

    Explicitly set workload api socket location
    Signed-off-by: Evan Gilman <evan@scytale.io>

--- 03-start-workload.sh
+++ 03-start-workload.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start ghostunnel
-/opt/ghostunnel/ghostunnel \
+/opt/ghostunnel/ghostunnel server \
         --listen=0.0.0.0:8443 \
         --target=127.0.0.1:8080 \
-        --use-workload-api-addr=unix:///opt/spire/spire_api \
+        --use-workload-api-addr=unix:///opt/spire/.data/workload_api \
         --allow-uri=spiffe://cluster1/ns/foo/sa/sleep
 
 # Start nc
